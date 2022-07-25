4b9a0f698a8dbdc514b1f074cc582d2c7fc81f32
Xuhang Cao <65797697+Xiaocao-Cxh@users.noreply.github.com>
2022-07-25 11:02:47 -0400

    Show Current Time

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,6 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+
+@app.route('/time')
+def now():
+    date = datetime.now()
+    return str(date)
 
 
 app.run(host='0.0.0.0',
